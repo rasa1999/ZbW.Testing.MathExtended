6cbaf51b86fdefd5911ddc669825adb959599cdd
Raphael Sager <raphael.sager99@bluewin.ch>
2023-01-02 11:22:52 +0100

    done

--- ZbW.Testing.MathExtended_Aufgabe/ZbW.Testing.MathExtended.UnitTests/ExtendedOperationTests.cs
+++ ZbW.Testing.MathExtended_Aufgabe/ZbW.Testing.MathExtended.UnitTests/ExtendedOperationTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Net;
+using NUnit.Framework;
 
 namespace ZbW.Testing.MathExtended.Library.UnitTests
 {
@@ -6,7 +8,53 @@ namespace ZbW.Testing.MathExtended.Library.UnitTests
     public class ExtendedOperationTests
     {
         [Test]
-        public void
+        public void Percent_Percent_Return10()
+        {
+            var sut = new ExtendedOperation();
 
+            var result = sut.Percent(20, 100);
+
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Exponent_Exponent_Return32()
+        {
+            var sut = new ExtendedOperation();
+
+            var result = sut.Exponent(2, 5);
+
+            Assert.That(result, Is.EqualTo(32));
+        }
+
+        [Test]
+        public void Factorial_Factorial_Return120()
+        {
+            var sut = new ExtendedOperation();
+
+            var result = sut.Factorial(5);
+
+            Assert.That(result, Is.EqualTo(120));
+        }
+
+        [Test]
+        public void Factorial_Factorial_ThrowOverFlowException()
+        {
+            var sut = new ExtendedOperation();
+
+            void Calculation() => sut.Factorial(15);
+
+            Assert.Throws<OverflowException>(Calculation, "Does not Throw the correct exception.");
+        }
+
+        [Test]
+        public void Modulo_Modulo_Return0()
+        {
+            var sut = new ExtendedOperation();
+
+            var result = sut.Modulo(10, 2);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
     }
 }
